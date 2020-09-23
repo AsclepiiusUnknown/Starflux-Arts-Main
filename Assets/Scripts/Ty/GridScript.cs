218864fb35714be9040d52509f16865817c06c8d
tylerwhelan <Tyler.Whelan@studytafensw.edu.au>
2020-09-23 12:55:52 +1000

    Beginnings of rudimentary pathfinding.

---
+++ Assets/Scripts/Ty/GridScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridScript : MonoBehaviour
+{
+    List<Transform> gridPoints;
+
+    public Vector3 GetNearestPoint(Vector3 vector)
+    {
+        int index = 0;
+        float distance = Vector3.Distance(vector, new Vector3(gridPoints[0].position.x, 0, gridPoints[0].position.y));
+        for (int i = 0; i < gridPoints.Count; i++)
+        {
+            if (Vector3.Distance(vector, new Vector3(gridPoints[i].position.x, 0, gridPoints[i].position.y)) <= distance)
+            {
+                index = i;
+            }
+        }
+
+        return new Vector3(gridPoints[index].position.x, 0, gridPoints[index].position.y);
+    }
+}
---
+++ Assets/Scripts/Ty/Pathfinder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+
+    public void FindPath(Vector3 position, Vector3 destination)
+    {
+
+    }
+}
