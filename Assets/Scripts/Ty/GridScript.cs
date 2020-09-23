5383274297ae9151e6b2705d42412886d14c2c03
tylerwhelan <Tyler.Whelan@studytafensw.edu.au>
2020-09-23 14:30:54 +1000

    Changed grid script.

--- Assets/Scripts/Ty/GridScript.cs
+++ Assets/Scripts/Ty/GridScript.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class GridScript : MonoBehaviour
 {
-    List<Transform> gridPoints;
+    //List<Transform> gridPoints;
+    public float gridDistance;
+    public float height;
+    public float width;
 
     public Vector3 GetNearestPoint(Vector3 vector)
     {
+        Vector3 mVec = new Vector3(Mathf.Clamp(vector.x, -((width / 2) * gridDistance), ((width / 2) * gridDistance)), 0, Mathf.Clamp(vector.z, -((height / 2) * gridDistance), ((height / 2) * gridDistance)));
+        mVec /= gridDistance;
+        mVec = new Vector3(Mathf.Round(mVec.x), 0, Mathf.Round(mVec.z));
+        mVec *= gridDistance;
+
+        /*
         int index = 0;
         float distance = Vector3.Distance(vector, new Vector3(gridPoints[0].position.x, 0, gridPoints[0].position.y));
         for (int i = 0; i < gridPoints.Count; i++)
@@ -17,7 +26,8 @@ public class GridScript : MonoBehaviour
                 index = i;
             }
         }
+        */
 
-        return new Vector3(gridPoints[index].position.x, 0, gridPoints[index].position.y);
+        return mVec;
     }
 }
