a745aa4c3f7330cdc16935fa8e3d61e87f5fb204
tylerwhelan <Tyler.Whelan@studytafensw.edu.au>
2020-09-23 15:33:06 +1000

    A little more on pathfinding.

--- Assets/Scripts/Ty/GridScript.cs
+++ Assets/Scripts/Ty/GridScript.cs
@@ -32,5 +32,20 @@ namespace Ty
 
             return mVec;
         }
+
+        public float CalculateValue(Vector2 position, Vector2 destination)
+        {
+            return Vector2.Distance(position, destination);
+        }
+
+        public List<Vector2> GetSurroundingPoints(Vector2 position)
+        {
+            // Currently unfinished
+            List<Vector2> list =  new List<Vector2>();
+            float posX = Mathf.Clamp(position.x, -((width / 2) * gridDistance), ((width / 2) * gridDistance));
+            float posY = Mathf.Clamp(position.y, -((height / 2) * gridDistance), ((height / 2) * gridDistance));
+            list.Add(new Vector2(0, 0));
+            return list;
+        }
     }
 }
--- Assets/Scripts/Ty/Pathfinder.cs
+++ Assets/Scripts/Ty/Pathfinder.cs
@@ -7,10 +7,18 @@ namespace Ty
     public class Pathfinder : MonoBehaviour
     {
         public GridScript gridRef;
+        List<NodeInfo> nodeList;
 
         public void FindPath(Vector3 position, Vector3 destination)
         {
             Vector3 inDestination = gridRef.GetNearestPoint(destination);
+
         }
+    }
+
+    public struct NodeInfo
+    {
+        float value;
+        Vector3 position;
     }
 }
